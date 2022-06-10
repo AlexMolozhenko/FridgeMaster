e972a505bbd378088e2e472ee85d1baa851f26b2
Alexsey Molozhenko <jukov20150@gmail.com>
2022-06-10 22:00:00 +0300

    locations migrate

---
+++ database/migrations/2022_06_10_141533_create_locations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->increments('id')->unsigned()->index();;
+            $table->string('title');
+            $table->integer('all_blocks');
+            $table->integer('remainder_blocks');
+            $table->dateTimeTz('timezone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('locations');
+    }
+}
