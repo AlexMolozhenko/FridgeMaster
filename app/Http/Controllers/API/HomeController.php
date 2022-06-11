79cd44120fb5125e083db24a38d739bda3402c0b
Alexsey Molozhenko <jukov20150@gmail.com>
2022-06-11 23:14:15 +0300

    homeController

--- app/Http/Controllers/API/HomeController.php
+++ app/Http/Controllers/API/HomeController.php
@@ -78,17 +78,85 @@ class HomeController extends ApiController
      *      ),
      *         @OA\Response(
      *           response="200",
-     *           description="information about the location with the number of blocks available ",
+     *           description="number of blocks and amount",
      *      ),
      *         @OA\Response(
      *          response="404",
-     *          description="There is no location for the given id."
+     *          description="not enough blocks"
      *      )
      * )
      *
      * @return Illuminate\Http\Response;
      */
     public function calculate(Request $request){
-        return \response()->json($request->all(),200);
+
+        $calculateRequest = $request->all();
+        $location = Location::where('id',$request->input('locationId'))->get();
+
+        $numberOfBlocks = ceil(($calculateRequest['volume'] / 2  ));
+
+        $getblock = Location::where('id',$request->input('locationId'))->get('remainder_blocks');
+        $block = $getblock[0]['remainder_blocks'];
+
+        if($block < $numberOfBlocks){
+            return response()->json([
+                'massage'=>'not enough blocks',
+                'remainder blocks'=>$block
+            ])->setStatusCode(404);
+        }
+
+
+        $d1 = $calculateRequest['dateTimeFrom'] ;
+        $d2 = $calculateRequest['dateTimeBy'];
+        $d1_ts = strtotime($d1);
+        $d2_ts = strtotime($d2);
+        $seconds = abs($d1_ts - $d2_ts);
+        $days = floor($seconds / 86400);
+
+        $storageCost = $days * $location[0]['blockPricePerDay'];
+
+        return response()->json([
+            'userId'=>$calculateRequest['userId'],
+            'locationId'=>$location[0]['id'],
+            'title'=>$location[0]['title'],
+            'blocks'=>$numberOfBlocks,
+            'dateTimeFrom'=>$calculateRequest['dateTimeFrom'],
+            'dateTimeBy'=>$calculateRequest['dateTimeBy'],
+            'temperature'=>$calculateRequest['temperature'],
+            'days'=>$days,
+            'storageCost'=>$storageCost
+        ])->setStatusCode(200);
     }
+
+
+    /**
+     *  @OA\Post (
+     *     path="/user/bookBlocks",
+     *     tags={"User"},
+     *     summary=" block booking.",
+     *     description="If the user agrees with the results of the calculator, he clicks on the booking button Book blocks",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(ref="#/components/schemas/bookblocksRequest")
+     *      ),
+     *      @OA\Response(
+     *           response="200",
+     *           description="booking confirmation and issuance of booking number",
+     *      ),
+     *        @OA\Response(
+     *          response="404",
+     *          description="this booking already exists"
+     *      )
+     *  )
+     * @return Illuminate\Http\Response;
+     */
+    public function bookBlocks(Request $request){
+
+
+        return response()->json([
+            'massage'=>'ok',
+            'data'=>$request->all()
+        ])->setStatusCode(200);
+    }
+
 }
